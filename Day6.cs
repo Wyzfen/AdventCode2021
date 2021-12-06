aa3ba5996d2fa7eff01b602154a9e47f3e673ffe
Wyzfen <anonuser42@hotmail.com>
2021-12-06 23:49:40 +0000

    day6

---
+++ Day6.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCode2021
+{
+    [TestClass]
+    public class Day6
+    {
+        readonly static int[] test = new int[]
+        {
+            3,4,3,1,2
+        };
+
+        readonly static string day = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name.ToLower();
+        readonly IEnumerable<int> values = Utils.FromString<int>(Utils.FromFile<string>($"{day}.txt").First(), ",");
+
+        [TestMethod]
+        public void Problem1()
+        {           
+            var dict = values.GroupBy(v => v).ToDictionary(k => k.Key, v => v.Count());
+            var input = Enumerable.Range(0, 9).Select(v => dict.TryGetValue(v, out int r) ? r : 0).ToArray();
+            for(int index = 1; index < 80; index++)
+            {
+                input[(index + 7) % 9] += input[index % 9]; // existing critters age 6
+            }
+
+            int result = input.Sum();
+
+            Assert.AreEqual(result, 360761);
+        }
+
+        [TestMethod]
+        public void Problem2()
+        {
+            var dict = values.GroupBy(v => v).ToDictionary(k => k.Key, v => (long) v.Count());
+            var input = Enumerable.Range(0, 9).Select(v => dict.TryGetValue(v, out long r) ? r : 0).ToArray();
+            for (int index = 1; index < 256; index++)
+            {
+                input[(index + 7) % 9] += input[index % 9]; // existing critters age 6
+            }
+
+            long result = input.Sum();
+
+            Assert.AreEqual(result, 360761);
+        }
+    }
+}
